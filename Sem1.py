9cb4c66d7606f9202a177fd61e83895513bc6b7b
DYK42 <yurandk42@gmail.com>
2022-09-23 21:09:58 +0700

    add Task #2

--- Sem1.py
+++ Sem1.py
@@ -5,13 +5,28 @@
 # - 7 -> да
 # - 1 -> нет
 
-def check_weekends(num):
-    if 5 < num < 8:
-        print('Да!')
-    elif 0 < num < 6:
-        print('Нет!')
-    else:
-        print('Превышает количество дней недели!')
+# def check_weekends(num):
+#     if 5 < num < 8:
+#         print('Да!')
+#     elif 0 < num < 6:
+#         print('Нет!')
+#     else:
+#         print('Превышает количество дней недели!')
 
-number = int(input('Введите целое число: '))
-check_weekends(number)
+# number = int(input('Введите целое число: '))
+#check_weekends(number)
+
+# 2
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def checkPredicate(x, y, z):
+    return (not (x or y or z)) == (not x and not y and not z)
+
+x = int(input('Введите целое число X: '))
+y = int(input('Введите целое число Y: '))
+z = int(input('Введите целое число Z: '))
+
+if checkPredicate(x, y, z):
+    print('Утверждение истинно!')
+else:
+    print('Утверждение ложно!')
