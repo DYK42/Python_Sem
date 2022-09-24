6ee0cc2c9f01f962e6ff7b4e81120c7d5cd9964d
DYK42 <yurandk42@gmail.com>
2022-09-24 14:44:17 +0700

    add Task #5

--- Sem1.py
+++ Sem1.py
@@ -65,19 +65,48 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон
 # возможных координат точек в этой четверти (x и y).
 
-def get_range_sector(num):
-    if 0 < num < 5:
-        match num:
-            case 1:
-                print('Диапазон (+x; +y)')
-            case 2:
-                print('Диапазон (-x; +y)')
-            case 3:
-                print('Диапазон (-x; -y)')
-            case 4:
-                print('Диапазон (+x; -y)')
-    else:
-        print("Превышение количества. Число может быть задано от 1 до 4.")
+# def get_range_sector(num):
+#     if 0 < num < 5:
+#         match num:
+#             case 1:
+#                 print('Диапазон (+x; +y)')
+#             case 2:
+#                 print('Диапазон (-x; +y)')
+#             case 3:
+#                 print('Диапазон (-x; -y)')
+#             case 4:
+#                 print('Диапазон (+x; -y)')
+#     else:
+#         print("Превышение количества. Число может быть задано от 1 до 4.")
+#
+# number = int(input('Введите номер четверти: '))
+# get_range_sector(number)
 
-number = int(input('Введите номер четверти: '))
-get_range_sector(number)
+# 5
+# Напишите программу, которая принимает на вход координаты двух точек и
+# находит расстояние между ними в 2D пространстве.
+
+
+def get_length_segment(a, b):
+    return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+
+def set_coordinates(num):
+    l = []
+    coord = ['X', 'Y']
+    for i in range(num):
+        while True:
+            try:
+                number = int(input(f"Введите координату по оси {coord[i]}: "))
+                l.append(number)
+                break
+            except ValueError:
+                print("Введите целое число!")
+    return l
+
+print("Введите координаты точки А")
+a = set_coordinates(2)
+
+print("Введите координаты точки B")
+b = set_coordinates(2)
+
+print(f"Расстояние между точками A и B равно: {format(get_length_segment(a, b), '.2f')}")
