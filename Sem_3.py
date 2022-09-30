296ce342fa403f824eb07e4aebf6036521996838
DYK42 <yurandk42@gmail.com>
2022-09-30 21:27:45 +0700

    add Sem_3.py

---
+++ Sem_3.py
@@ -0,0 +1,27 @@
+# print(sum(int(i) for i in input('Введите вещественное число: ') if i.isdigit()))
+
+# num = float(input('N: '))
+# while int(num) != num:
+#     num *= 10
+#
+# result = 0
+# while num:
+#     result += num % 10
+#     num //= 10
+#
+# print(result)
+#
+# def fib(N):
+#     last = 1
+#     for i in range(1, N + 1): # 1 * 2 * 3 * 4
+#     last *= i
+#     yield last
+#
+#
+# N = int(input('N: '))
+# lst = list(fib(N))
+#
+# print(lst)
+# print(lst)
+
+
