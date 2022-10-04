45ea50de8f8b4e2eab1ba10c1ee98affb7099408
DYK42 <yurandk42@gmail.com>
2022-10-05 00:17:19 +0700

    modification my_func.py

--- my_func.py
+++ my_func.py
@@ -4,11 +4,18 @@ import string
 
 
 def get_input(mode='i'):
+    value = ''
     match mode:
         case 'i':
-            return int(input('Введите целое число: '))
+            value = input('Введите целое число: ')
+            if not value.isdigit():
+                return print('Введено не целое число!')
+            return int(value)
         case 'f':
-            return float(input('Введите вещественное число: '))
+            value = input('Введите вещественное число: ')
+            if not value.replace('.', '', 1).isdigit():
+                return print('Введено не вещественное число!')
+            return float(value)
         case 's':
             return input('Введите текст: ')
         case _:
@@ -16,14 +23,29 @@ def get_input(mode='i'):
 
 
 def get_list(mode='i'):
-    num = int(input('Введите размер списка: '))
     match mode:
         case 'i':
+            num = int(input('Введите размер списка: '))
             return list(range(num))
         case 'f':
+            num = int(input('Введите размер списка: '))
             return [round(random.random() + random.randint(0, 100), 2) for i in range(num)]
         case 's':
+            num = int(input('Введите размер списка: '))
             return [''.join(random.choice(string.ascii_letters) for i in range(random.randrange(0, 5))) for j in
                     range(num)]
+        case 'h':
+            l = []
+            while True:
+                value = input('Введите элемент списка (Пустое значение для выхода): ')
+                if value.isdigit():
+                    l.append(int(value))
+                elif value.replace('.', '', 1).isdigit():
+                    l.append(float(value))
+                else:
+                    if not value:
+                        return l
+                    else:
+                        l.append(value)
         case _:
             print('Настройка не задана!')
